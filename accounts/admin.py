c3f166ec5dc53ac812f00af546acfff46f101fe5
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2025-07-17 21:58:33 +0300

    added custom user db

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-# Register your models here.
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
+
+    def clean_password2(self):
+        pw1 = self.cleaned_data.get("password1")
+        pw2 = self.cleaned_data.get("password2")
+        if pw1 and pw2 and pw1 != pw2:
+            raise forms.ValidationError("Passwords don't match")
+        return pw2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+class CustomUserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password', 'is_active', 'is_staff')
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'lastname', 'role', 'plant')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
