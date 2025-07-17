bd3b132a360ebf22cf6bd0f83690eb50ff055b69
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2025-07-18 00:00:38 +0300

    pfp and names

--- home/views.py
+++ home/views.py
@@ -6,8 +6,21 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 
 
+
 @method_decorator(login_required(login_url='/authentication/login/'), name='dispatch')
 @method_decorator(never_cache, name='dispatch')
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/index.html')
+        user = request.user
+        student = user.student_profile
+        first_name_initial = student.user.name[0]
+        lastname = student.user.lastname
+        pfp = student.profile_picture
+
+        context = {
+            'first_name_initial': first_name_initial,
+            'lastname': lastname,
+            'pfp': pfp
+        }
+
+        return render(request, 'home/index.html', context=context)
